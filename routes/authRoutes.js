86b087f1c28b89f1ffd4bbed5f7c1570f264d3ed
SnowSurfer <drinkaziu@gmail.com>
2019-07-07 15:23:33 -0400

    enable cookies and link to passport

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,11 @@ module.exports = app => {
   app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
   }));
+
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  //test to make sure we have access to user and auth is working
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 }
