b121bcc26c77fb950c40b9735536a969a80b121c
SnowSurfer <drinkaziu@gmail.com>
2019-07-07 13:25:46 -0400

    wire up passport w/ google strategy

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('RX Life')
-});
-
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, () => {
+    console.log(accessToken);
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`RX Life server running on port ${PORT}`)
-})
+});
