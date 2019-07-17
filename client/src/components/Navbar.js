ed6f4d66411cec4f05f4ed63b0717ca9467b93cb
SnowSurfer <drinkaziu@gmail.com>
2019-07-17 14:52:32 -0400

    finish wiring up auth to frontend

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,34 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo2 from '../img/logo2.png';
 import { NavLink } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
-function Navbar() {
-  return (
-    <div className="header">
-      <div className="header-content">
-        <NavLink to='/' className="header-content-logo">
-          <span>The</span>
-          <img src={logo2} alt="rx-life-logo"/>
-          <span>Life</span>
-        </NavLink>
-        <div className="header-content-links">
-          <HashLink to='/#challenge-section'>The Challenge</HashLink>
-          <NavLink to='/points'>The Points</NavLink>
-          <a href="/points#how-it-works-section">How to Play</a>
-          <a href="#about">About</a>
-          <a href="#account">Account</a>
-        </div>
-        <div className="header-content-auth">
-          <NavLink to='/signup'>
-            <FontAwesomeIcon icon={faUserCircle}/>
+class Navbar extends Component {
+  render() {
+
+    const { auth } = this.props;
+    return (
+      <div className="header">
+        <div className="header-content">
+          <NavLink to='/' className="header-content-logo">
+            <span>The</span>
+            <img src={logo2} alt="rx-life-logo"/>
+            <span>Life</span>
           </NavLink>
+          <div className="header-content-links">
+            <HashLink to='/#challenge-section'>The Challenge</HashLink>
+            <NavLink to='/points'>The Points</NavLink>
+            <a href="/points#how-it-works-section">How to Play</a>
+            <a href="#about">About</a>
+            <a href="#account">Account</a>
+          </div>
+          <div className="header-content-auth">
+            {auth ?
+              <a href='/api/logout'>
+                hi, {auth.firstName}
+              </a> :
+              <a href='/auth/google'>
+                <FontAwesomeIcon icon={faUserCircle} />
+              </a>
+            }
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default Navbar;
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Navbar);
