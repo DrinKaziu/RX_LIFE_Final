0057b3dcbc7cec7f8f31b894ac3e0ec7f37db128
SnowSurfer <drinkaziu@gmail.com>
2019-07-17 16:24:03 -0400

    set up Stripe API keys in frontend

--- client/src/index.js
+++ client/src/index.js
@@ -21,6 +21,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+console.log('Stripe Key is: ', process.env.REACT_APP_STRIPE_KEY);
+console.log('Environment is: ', process.env.NODE_ENV);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
