4b4b0c5c806e060ebba4e8a328bfac042f49bf4b
SnowSurfer <drinkaziu@gmail.com>
2019-07-17 13:52:27 -0400

    create fetchUser action - wire up with thunk

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from './actions';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Points from './components/Points';
@@ -7,19 +9,25 @@ import Signup from './components/Signup';
 import Ads from './components/Ads';
 import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/points' component={Points}/>
-        <Route exact path='/signup' component={Signup} />
-      </Switch>
-      <Ads />
-      <Footer />
-    </div>  
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/points' component={Points}/>
+          <Route exact path='/signup' component={Signup} />
+        </Switch>
+        <Ads />
+        <Footer />
+      </div>  
+    );
+  }
 }
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import './css/index.css';
 import './sass/main.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
