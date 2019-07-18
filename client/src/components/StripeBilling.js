fe50258c67cfd2a2d0ace08abb05f8a2c6dc1fa6
SnowSurfer <drinkaziu@gmail.com>
2019-07-18 12:54:40 -0400

    add action creator for stripe billing

--- client/src/components/StripeBilling.js
+++ client/src/components/StripeBilling.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 import StripeCheckout from 'react-stripe-checkout';
+import Button from '@material-ui/core/Button';
 
 class StripeBilling extends Component {
   render() {
     return (
       <StripeCheckout 
+        name={'RX Life Challenge'}
+        description={'READY! SET! GO!'}
         amount={4500}
-        token={token => console.log(token)}
+        token={token => this.props.handleStripeToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <Button variant="outlined" color="secondary">Get the Challenge!</Button>
+      </StripeCheckout>
     )
   }
 }
 
-export default StripeBilling; 
+export default connect(null, actions)(StripeBilling); 
